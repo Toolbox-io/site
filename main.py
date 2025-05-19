5f5e3287d73fdca918a829f486ce3851886f1cd3
denis0001-dev <denis0001.dev@ya.ru>
2025-05-19 19:38:33 +0300

    Сервер
    Signed-off-by: denis0001-dev <denis0001.dev@ya.ru>

---
+++ main.py
@@ -0,0 +1,74 @@
+import mimetypes
+import os
+
+from flask import Flask, send_from_directory, redirect
+
+app = Flask(__name__)
+
+# Путь к папке с контентом
+CONTENT_PATH = 'src'
+
+
+# Функция для поиска файла с учетом разных расширений
+def find_file(path: str) -> tuple[str | None, str | None]:
+    # Полный путь относительно CONTENT_PATH
+    full_path = os.path.join(CONTENT_PATH, path)
+
+    # Если это директория и в ней есть index.html
+    if os.path.isdir(full_path):
+        index_file = os.path.join(full_path, 'index.html')
+        if os.path.isfile(index_file):
+            if not path.endswith("/"): return index_file, f"{path}/"
+            else: return index_file, None
+        # Если index.html нет, ищем <dir>.html в родительской папке
+        dir_html = os.path.join(os.path.dirname(full_path), os.path.basename(path) + '.html')
+        if os.path.isfile(dir_html):
+            return dir_html, None
+        return None, None
+
+    # Если это файл без расширения
+    if os.path.isfile(full_path):
+        return full_path, None
+
+    # Если есть файл с расширением .html
+    html_path = os.path.join(CONTENT_PATH, path + '.html')
+    if os.path.isfile(html_path):
+        return html_path, None
+
+    # Проверяем все подкаталоги
+    for dirpath, dirnames, filenames in os.walk(CONTENT_PATH):
+        for filename in filenames:
+            if filename.startswith(path):
+                return os.path.join(dirpath, filename), None
+
+    return None, None
+
+
+# Обработчик всех запросов
+@app.route('/<path:path>')
+def serve_files(path):
+    file = find_file(path)
+    file_path = file[0]
+    # If it's a directory and URL doesn't end with '/', redirect
+    if file[1]: return redirect(file[1])
+    
+    if file_path:
+        # Определяем тип контента по расширению
+        mime_type = mimetypes.guess_type(file_path)[0]
+        return send_from_directory(
+            os.path.dirname(file_path),
+            os.path.basename(file_path),
+            mimetype=mime_type
+        )
+
+    return "404 Not Found", 404
+
+
+# Обработчик для корневой директории
+@app.route('/')
+def index():
+    return send_from_directory(CONTENT_PATH, 'index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
