81d3f01ed0be16c688280598df498adf53590d78
denis0001-dev <denis0001.dev@ya.ru>
2025-06-23 13:44:58 +0300

    Simplify Caddy  to just build the container without an image
    Signed-off-by: denis0001-dev <denis0001.dev@ya.ru>

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# 1. cd to the right directory
 set -e
 cd "$(dirname "$0")" || exit
+
+# 2. Build
 COMPOSE_BAKE=true docker compose build
 
+# 3. Run
 if ! [[ $NORUN == "true" ]]; then
   cd server/scripts || exit
   chmod +x run.sh
